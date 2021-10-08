ed524f598462f77dcda574810c95787229db81a0
Emjr31 <adonay.chacon12@gmail.com>
2021-10-08 10:23:10 -0600

    Clase producto

---
+++ EX2-POO/Productos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2_POO
+{
+    class Productos
+    {
+        string codigo;
+        string nombre;
+        float precio;
+
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public float Precio { get => precio; set => precio = value; }
+    }
+}
